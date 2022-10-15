881b66d1bf93fa2ee5fd6b689b386738db41a111
kngako <kpangako+github@gmail.com>
2022-10-16 01:37:48 +0200

    No escape fix

---
+++ test/character-support.test.js
@@ -0,0 +1,23 @@
+const YAML = require("yaml")
+const fs = require("fs")
+const { expect } = require('@jest/globals');
+const i18nUtil = require("../lib/i18n-util")
+
+
+describe("Make sure character set is utf-8", () => {
+    // Read all files in locales...
+    test(`equal and slash`, () => {
+        expect(i18nUtil.getText("en", "enterPinToSend", {amountText: "1 sats /(1 sats = 0.00000001 BTC)", receiver: "yourself"})).toBe("Enter your Machankura PIN to send 1 sats /(1 sats = 0.00000001 BTC) to yourself:")
+    })
+    
+
+    
+});
+
+// test("No Duplicate Keys all files", async () => {
+//     // Read all files in locales...
+//     const file = fs.readFileSync('./locales/en.yaml', 'utf8')
+//     const en = YAML.parse(file)
+
+//     expect()
+// });
